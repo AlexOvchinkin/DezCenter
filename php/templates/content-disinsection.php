93b5eed7b37108a80d249f74cd6ad54a18e71520
unknown <al.ovchinkin@gmail.com>
2018-08-02 16:21:57 +0300

    section qualities

--- php/templates/content-disinsection.php
+++ php/templates/content-disinsection.php
@@ -1,4 +1,100 @@
-<section class="h-50 text-success" style="z-index: 1000;">
-  <p class="h1 pt-5 text-center">Страница находится</p>
-  <p class="h3 m-0 p-0 pb-5 text-center">в разработке</p>
+<section class="qualities">
+  <div class="container">
+    <p class="qualities__title">
+      Избавим ваш дом, офис, ресторан от надоедливых насекомых за одну обработку.
+    </p>
+    <p class="qualities__subtitle">
+      ОТ 1500 РУБ. С ВЫЕЗДОМ В ДЕНЬ ЗАЯВКИ
+    </p>
+
+    <div id="qualitiesCarousel" class="carousel d-md-none slide qualities__carousel" data-ride="carousel" data-interval="0">
+      <div class="carousel-inner qualities__items-wrapper">
+        <div class="carousel-item qualities__item active">
+          <div class="qualities__divider"></div>
+          <img class="qualities__img" src="img/disinsection/award.png" alt="award">
+          <div class="carousel-caption qualities__caption qualities__caption-short">
+            <p class="qualities__caption">Гарантия на работу до 3х лет</p>
+          </div>
+        </div>
+        <div class="carousel-item qualities__item">
+          <div class="qualities__divider"></div>
+          <img class="qualities__img" src="img/disinsection/car.png" alt="car">
+          <div class="carousel-caption qualities__caption">
+            <p class="qualities__caption">Мастер выезжает через 5 минут после звонка</p>
+          </div>
+        </div>
+        <div class="carousel-item qualities__item">
+          <div class="qualities__divider"></div>
+          <img class="qualities__img" src="img/disinsection/pig.png" alt="pig">
+          <div class="carousel-caption qualities__caption">
+            <p class="qualities__caption">Бесплатная диагностика при заказе услуги у нас</p>
+          </div>
+        </div>
+        <div class="carousel-item qualities__item">
+          <div class="qualities__divider"></div>
+          <img class="qualities__img" src="img/disinsection/shield.png" alt="shield">
+          <div class="carousel-caption qualities__caption">
+            <p class="qualities__caption">Только безопасные для человека химвещества</p>
+          </div>
+        </div>
+        <div class="carousel-item qualities__item">
+          <div class="qualities__divider"></div>
+          <img class="qualities__img" src="img/disinsection/stopwatch.png" alt="stopwatch">
+          <div class="carousel-caption qualities__caption qualities__caption-wide">
+            <p class="qualities__caption">Работаем оперативно, сводя неудобства к минимуму</p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev qualities__control" href="#qualitiesCarousel" role="button" data-slide="prev">
+        <img src="img/disinsection/left-arrow.png" alt="arrow">
+      </a>
+      <a class="carousel-control-next qualities__control" href="#qualitiesCarousel" role="button" data-slide="next">
+        <img src="img/disinsection/right-arrow.png" alt="arrow">
+      </a>
+    </div>
+
+    <!-- MD up resolution -->
+    <div class="qualities__table d-none d-md-block">
+      <div class="row qualities__table--first-row">
+        <div class="col-4">
+          <div class="qualities__img-wrapper">
+            <div class="qualities__table-divider"></div>
+            <img class="qualities__table-img" src="img/disinsection/award.png" alt="award">
+          </div>
+          <p class="qualities__table-caption">Гарантия на работу до 3х лет</p>
+        </div>
+        <div class="col-4">
+          <div class="qualities__img-wrapper">
+            <div class="qualities__table-divider"></div>
+            <img class="qualities__table-img" src="img/disinsection/award.png" alt="award">
+          </div>
+          <p class="qualities__table-caption">Мастер выезжает через 5 минут после звонка</p>
+        </div>
+        <div class="col-4">
+          <div class="qualities__img-wrapper">
+            <div class="qualities__table-divider"></div>
+            <img class="qualities__table-img" src="img/disinsection/award.png" alt="award">
+          </div>
+          <p class="qualities__table-caption">Бесплатная диагностика при заказе услуги у нас</p>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-4">
+          <div class="qualities__img-wrapper">
+            <div class="qualities__table-divider"></div>
+            <img class="qualities__table-img" src="img/disinsection/award.png" alt="award">
+          </div>
+          <p class="qualities__table-caption">Только безопасные для человека химвещества</p>
+        </div>
+        <div class="col-4">
+          <div class="qualities__img-wrapper">
+            <div class="qualities__table-divider"></div>
+            <img class="qualities__table-img" src="img/disinsection/award.png" alt="award">
+          </div>
+          <p class="qualities__table-caption">Работаем оперативно, сводя неудобства к минимуму</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <button class="btn qualities__count-btn">Рассчитать цену</button>
 </section>
