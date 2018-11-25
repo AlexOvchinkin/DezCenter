f86911f522445d20bea110b09c836e20a948b3cd
ovchinkin aleksey <al.ovchinkin@gmail.com>
2018-11-25 20:33:36 +0300

    bug fix

--- php/lib/functions.php
+++ php/lib/functions.php
@@ -5,7 +5,7 @@ function getHeader($page) {
 }
 
 function getStylesheet($page) {
-  return '<link rel="stylesheet" href="css/'.$page.'/styles.css?3.1">';
+  return '<link rel="stylesheet" href="css/'.$page.'/styles.css?3.2">';
 }
 
 function getTemplate($template) {
